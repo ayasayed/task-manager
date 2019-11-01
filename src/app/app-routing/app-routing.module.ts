f4959b9c38b0109564a4ec08965fe153e4afd645
ayasayed <ayasayed_711@yahoo.com>
2019-11-02 01:06:28 +0200

    1 file modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from '../main/home/home.component' ;
-import {AddTaskComponent} from '../main/add-task/add-task.component' ;
+import {HomeComponent} from './main/home/home.component' ;
+import {AddTaskComponent} from 'add-task.component' ;
 import {TaskComponent} from '../main/task/task.component' ;
 import {NotFoundComponent} from '../' ;
 
